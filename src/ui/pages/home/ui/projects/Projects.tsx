3335fc4de495f48db024183ae4da84d13b56a709
Jose Toro <ing.josetoro.2@gmail.com>
2024-03-25 16:22:52 -0500

    ✉ Google maps and Send email features

--- src/ui/pages/home/ui/projects/Projects.tsx
+++ src/ui/pages/home/ui/projects/Projects.tsx
@@ -1,14 +1,18 @@
+"use client";
 import Link from "next/link";
 import Style from "./Projects.module.scss";
 import Utils from "@/ui/theme/application/utils/Utils.module.scss";
 import Image from "next/image";
+import { useState } from "react";
 
 const Projects = () => {
+	const [showMore, setShowMore] = useState(false);
+
 	return (
 		<>
 			<section className={`${Style.wrapper}`}>
 				<div
-					className={`${Utils.container} ${Utils.d_flex} ${Utils.flex_column}`}
+					className={`${Utils.container} ${Utils.d_flex} ${Utils.flex_column} ${Utils.gap_sm}`}
 				>
 					<p className={`${Utils.text_italic}`}>
 						Explora nuestra galería de proyectos, los cuales capturan la
@@ -17,7 +21,7 @@ const Projects = () => {
 					</p>
 
 					<Link
-						className={`${Utils.button_outline_light} ${Utils.ml_auto} ${Utils.mt_sm}`}
+						className={`${Utils.button_outline_light} ${Utils.link_outline_off} ${Utils.ml_auto}`}
 						href="/portafolio"
 					>
 						Ver Portafolio
@@ -25,54 +29,70 @@ const Projects = () => {
 				</div>
 			</section>
 
-			<section className={`${Style.wrapper_projects}`}>
-				<h2 className={`${Utils.text_center} ${Utils.mb_md}`}>
-					Proyectos Destacados
-				</h2>
+			<section className={`${Utils.m_md} ${Utils.d_grid} ${Utils.gap_md}`}>
+				<div className={`${Style.wrapper_projects}`}>
+					<h2 className={`${Utils.text_center} ${Utils.mb_md}`}>
+						Proyectos Destacados
+					</h2>
 
-				<div className={`${Style.projects}`}>
-					{[1, 2, 3, 4, 5, 6].map((_, index) => (
-						<div key={index}>
-							<Image
-								src="/assets/images/highlighted-projects/142.jpg"
-								alt="Demo"
-								width={2160}
-								height={2160}
-								quality={70}
-							/>
+					<div className={`${Style.projects}`}>
+						{[1, 2, 3, 4, 5, 6].map((_, index) => (
+							<div key={index}>
+								<Image
+									src="/assets/images/highlighted-projects/142.jpg"
+									alt="Demo"
+									width={2160}
+									height={2160}
+									quality={70}
+								/>
 
-							<p className={`${Utils.text_center} ${Utils.mt_sm}`}>
-								Crystal Living
-							</p>
-						</div>
-					))}
+								<p className={`${Utils.text_center} ${Utils.mt_sm}`}>
+									Crystal Living
+								</p>
+							</div>
+						))}
 
-					{[1, 2, 3, 4, 5, 6].map((_, index) => (
-						<div key={index}>
-							<Image
-								src="/assets/images/highlighted-projects/142.jpg"
-								alt="Demo"
-								width={2160}
-								height={2160}
-								quality={70}
-							/>
+						{showMore &&
+							[1, 2, 3, 4, 5, 6].map((_, index) => (
+								<div key={index}>
+									<Image
+										src="/assets/images/highlighted-projects/142.jpg"
+										alt="Demo"
+										width={2160}
+										height={2160}
+										quality={70}
+									/>
 
-							<p className={`${Utils.text_center} ${Utils.mt_sm}`}>
-								Crystal Living
-							</p>
-						</div>
-					))}
+									<p className={`${Utils.text_center} ${Utils.mt_sm}`}>
+										Crystal Living
+									</p>
+								</div>
+							))}
+					</div>
+				</div>
+
+				<div
+					className={`${Utils.container} ${Utils.size_full} ${Utils.d_flex}`}
+				>
+					{!showMore && (
+						<button
+							className={`${Utils.button_outline_light} ${Utils.ml_auto}`}
+							onClick={() => setShowMore(true)}
+						>
+							Ver Más
+						</button>
+					)}
+
+					{showMore && (
+						<Link
+							href="/portafolio"
+							className={`${Utils.button_outline_light} ${Utils.ml_auto}`}
+						>
+							Ver Todos
+						</Link>
+					)}
 				</div>
 			</section>
-
-			<div className={`${Utils.container} ${Utils.d_flex}`}>
-				<Link
-					href="/portafolio"
-					className={`${Utils.button_outline_light} ${Utils.ml_auto}`}
-				>
-					Ver todos
-				</Link>
-			</div>
 		</>
 	);
 };
